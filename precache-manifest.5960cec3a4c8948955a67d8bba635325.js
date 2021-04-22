dc02c7600d34aa55289648439f4e9370b1c30722
fabemiliano <fab.emiliano@gmail.com>
2021-04-22 18:15:07 -0300

    deploy

---
+++ precache-manifest.5960cec3a4c8948955a67d8bba635325.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625ff4f05d044fcdaaa2",
+    "url": "/ilia-challenge-vue/css/app.2ca6e777.css"
+  },
+  {
+    "revision": "14540979a6685ab378bf0cee1e97f942",
+    "url": "/ilia-challenge-vue/icons/bug.svg"
+  },
+  {
+    "revision": "b7d4f57872507c79b1ecd192aad61357",
+    "url": "/ilia-challenge-vue/icons/dark.svg"
+  },
+  {
+    "revision": "18a1dbc9e3a69ba77e890268451026e4",
+    "url": "/ilia-challenge-vue/icons/dragon.svg"
+  },
+  {
+    "revision": "68b142da814c55f9225db2c16193ba01",
+    "url": "/ilia-challenge-vue/icons/electric.svg"
+  },
+  {
+    "revision": "603e08d1937f16395d2d304ef6d30ab7",
+    "url": "/ilia-challenge-vue/icons/fairy.svg"
+  },
+  {
+    "revision": "8ce4fa81f8f29f8c6871b88f78916637",
+    "url": "/ilia-challenge-vue/icons/fighting.svg"
+  },
+  {
+    "revision": "33d14b307cd671ab9c1ca76f4788df7a",
+    "url": "/ilia-challenge-vue/icons/fire.svg"
+  },
+  {
+    "revision": "8c1e094fa4fa86cf9a4b943d6b6c9253",
+    "url": "/ilia-challenge-vue/icons/flying.svg"
+  },
+  {
+    "revision": "ff3973459922626c46b8935bd3517deb",
+    "url": "/ilia-challenge-vue/icons/ghost.svg"
+  },
+  {
+    "revision": "d589dd00e175efada7294faf05f52ac6",
+    "url": "/ilia-challenge-vue/icons/grass.svg"
+  },
+  {
+    "revision": "289c4927e264d15f5be41d5c53f5c601",
+    "url": "/ilia-challenge-vue/icons/ground.svg"
+  },
+  {
+    "revision": "f82b7e5d85efb43a4e5001ed490269d4",
+    "url": "/ilia-challenge-vue/icons/ice.svg"
+  },
+  {
+    "revision": "8f2a7aaa9ae7bd49980f1b2b99a2577f",
+    "url": "/ilia-challenge-vue/icons/normal.svg"
+  },
+  {
+    "revision": "183135e96caba5df8c380e0d2acdea6a",
+    "url": "/ilia-challenge-vue/icons/poison.svg"
+  },
+  {
+    "revision": "60982f05e13fbd9b6e6d96ce3456d365",
+    "url": "/ilia-challenge-vue/icons/psychic.svg"
+  },
+  {
+    "revision": "499a0fa10362807e9a3218082be3d288",
+    "url": "/ilia-challenge-vue/icons/rock.svg"
+  },
+  {
+    "revision": "95342da3d89b3644b12d596a41d631c7",
+    "url": "/ilia-challenge-vue/icons/steel.svg"
+  },
+  {
+    "revision": "e29fb8e379f9ed5335a02eb3881d25d6",
+    "url": "/ilia-challenge-vue/icons/water.svg"
+  },
+  {
+    "revision": "6322b09fb5751a1a12576883613c33fe",
+    "url": "/ilia-challenge-vue/img/br.6322b09f.png"
+  },
+  {
+    "revision": "e27b7d18b01ae4765133f4aec4aaf61d",
+    "url": "/ilia-challenge-vue/img/pokeball.e27b7d18.png"
+  },
+  {
+    "revision": "0de2e412c56f6c0aa7e1df516f426e08",
+    "url": "/ilia-challenge-vue/img/uk.0de2e412.png"
+  },
+  {
+    "revision": "e13b3569dfe61cd653ae921cdb443511",
+    "url": "/ilia-challenge-vue/index.html"
+  },
+  {
+    "revision": "625ff4f05d044fcdaaa2",
+    "url": "/ilia-challenge-vue/js/app.a2aa59de.js"
+  },
+  {
+    "revision": "8afa0898c94c0ca32249",
+    "url": "/ilia-challenge-vue/js/chunk-vendors.f9264722.js"
+  },
+  {
+    "revision": "37ddd9b7af0aa14383e20d9312a59cc2",
+    "url": "/ilia-challenge-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ilia-challenge-vue/robots.txt"
+  }
+]);
